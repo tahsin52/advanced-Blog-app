dc92143e3d01bf7fe8deb2156788fcbe466d8953
tahsin52 <tahsin_c@hotmail.com>
2021-08-12 21:29:27 +0300

    added post model and home

--- src/models.py
+++ src/models.py
@@ -7,5 +7,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(155), unique=True)
     username = db.Column(db.String(155), unique=True)
     password = db.Column(db.String(155))
+    posts = db.relationship('Post', backref='user', passive_delete=True, default=func.now())
+
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
 
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
--- src/views.py
+++ src/views.py
@@ -1,9 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import current_user
+
+from src import db
+from src.models import Post
 
 views = Blueprint("views", __name__)
 
 @views.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html',
-                           name=current_user)
+                           name=current_user,
+                           posts=posts)
+
+@views.route("/create-post", methods=["GET", "POST"])
+def create_post():
+    if request.method == "POST":
+        text = request.form.get('text')
+
+        if not text:
+            flash('Post cannot be empty', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+
+            flash('Post Created', category='success')
+            return redirect(url_for('views.home'))
